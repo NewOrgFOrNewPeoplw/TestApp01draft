d9ca8e6cbb3be977275adae739c8560f1265be7c
ajinkyab0000 <140574712+ajinkyab0000@users.noreply.github.com>
2023-07-27 20:01:18 -0700

    First Checkin

--- TestApp01/Program.cs
+++ TestApp01/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to DevOps World");
+Console.WriteLine("Welcome to Azure Devops Ajinkya");
 Console.ReadLine();
 
